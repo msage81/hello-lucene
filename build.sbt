bd30124c742c09a05a33d85f813d0f06363c58b7
Mark Sage <msage@rewardsnetwork.com>
2016-07-06 19:27:49 -0500

    initial check in

---
+++ build.sbt
@@ -0,0 +1,16 @@
+name := "hello-lucene"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+// https://mvnrepository.com/artifact/org.apache.lucene/lucene-core
+libraryDependencies += "org.apache.lucene" % "lucene-core" % "6.1.0"
+
+// https://mvnrepository.com/artifact/org.apache.lucene/lucene-analyzers-common
+libraryDependencies += "org.apache.lucene" % "lucene-analyzers-common" % "6.1.0"
+
+// https://mvnrepository.com/artifact/org.apache.lucene/lucene-queryparser
+libraryDependencies += "org.apache.lucene" % "lucene-queryparser" % "6.1.0"
+
+    
---
+++ src/main/scala/com/sausage/lucene/app/FirstApp.scala
@@ -0,0 +1,44 @@
+package com.sausage.lucene.app
+
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer
+import org.apache.lucene.document.{Document, Field, TextField}
+import org.apache.lucene.index.{DirectoryReader, IndexWriter, IndexWriterConfig, Term}
+import org.apache.lucene.queryparser.classic.QueryParser
+import org.apache.lucene.search.{IndexSearcher, ScoreDoc, TopDocs}
+import org.apache.lucene.store.RAMDirectory
+
+object FirstApp extends App{
+
+  val analyzer = new WhitespaceAnalyzer()
+  val directory = new RAMDirectory()
+  val config = new IndexWriterConfig( analyzer)
+  val indexWriter = new IndexWriter(directory, config)
+
+
+  val doc = new Document()
+  val text = "Lucene is an Information Retrieval library written in Java."
+  doc.add(new TextField("fieldname", text, Field.Store.YES))
+
+  indexWriter.addDocument(doc)
+  indexWriter.close()
+
+  val indexReader = DirectoryReader.open(directory)
+  val indexSearcher = new IndexSearcher(indexReader)
+
+  val parser = new QueryParser("fieldname", analyzer)
+  val query = parser.parse("Lucene")
+
+  val hitsPerPage = 10
+  val docs = indexSearcher.search(query, hitsPerPage)
+  val hits = docs.scoreDocs
+  val end = Math.min(docs.totalHits, hitsPerPage)
+  println("Total Hits: " + docs.totalHits)
+  println("Results: ")
+  for (i <- (0 until end)) {
+    val d = indexSearcher.doc(hits(i).doc)
+    println("Content: " + d.get("fieldname"))
+  }
+
+//  indexWriter.deleteDocuments(new Term("id", "1"))
+//  indexWriter.close()
+}
